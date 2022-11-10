3b3a3d6fb97a85c48f1ccc430fcf2945243a0d12
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-10 14:53:44 +0100

    aggiunto console.log per if-else

--- script.js
+++ script.js
@@ -11,27 +11,31 @@ let minore = prezzo - scontoMinore
 let over = prezzo - scontoOver
 */
 
+console.log(
+    `
+    ------Dati del passeggero------
+        Chilometri da percorrere: ${km}
+        Età del passeggero: ${eta}
+    ------Prezzo pieno------
+        Prezzo pieno: ${prezzo}
+    ------Prezzo Scontato (dove disponibile)------
+    `
+);
+
+
 if(eta < 18){
     minore = prezzo - scontoMinore
+    console.log ("Prezzo con sconto minorenne: " + minore + " €");
 } else if(eta >= 65){
     over = prezzo - scontoOver
+    console.log ("Prezzo con sconto over 65: " + over + " €");
 } else{
-    prezzo
+    console.log ("Prezzo totale: " + prezzo + " €");
 }
 
 
 
 /*
-console.log(
-    `
-    -------Dati del passeggero------
-        Chilometri da percorrere: ${km}
-        Età del passeggero: ${eta}
-    ------Totale--------
-        Prezzo totale: ${prezzo}
-    ------Prezzo Scontato (dove disponibile)--------
-        Minorenne: ${minore}
-        Over 65: ${over}
-    `
-);
+Minorenne: ${minore}
+Over 65: ${over}
 */
