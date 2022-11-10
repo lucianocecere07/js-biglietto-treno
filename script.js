cc2bc12e9fcd4eee1ae6197c4841070c99699209
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-10 14:22:35 +0100

    Inserimenot dei dati (prompt)

---
+++ script.js
@@ -0,0 +1,22 @@
+let km = prompt("Inserire quanti chilometri da percorrere");
+let eta = prompt("Inserire età del passeggero");
+
+let prezzo = km * 0.21
+
+
+
+
+/*
+console.log(
+    `
+    -------Dati del passeggero------
+        Chilometri da percorrere: ${km}
+        Età del passeggero: ${età}
+    ------Totale--------
+        Prezzo totale: ${prezzo}
+    ------Prezzo Scontato (dove disponibile)--------
+        Minorenne: ${minore}
+        Over 65: ${over}
+    `
+);
+*/
