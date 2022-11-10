2f5a91efade69745dc283d9c27129d58e306b234
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-10 17:39:31 +0100

    corretto puntggiatura  e if prima di console.log

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let km = prompt("Inserire quanti chilometri da percorrere");
 let eta = prompt("Inserire età del passeggero");
 
-let prezzo = km * 0.21
-let prezzoDecimali = prezzo.toFixed(2)
+let prezzo = km * 0.21;
+let prezzoDecimali = prezzo.toFixed(2);
 
-let scontoMinore = prezzo * 20 / 100
-let scontoOver = prezzo * 40 / 100
+let scontoMinore = prezzo * 20 / 100;
+let scontoOver = prezzo * 40 / 100;
+
+
+if (eta < 18) {
+    minore = prezzo - scontoMinore;
+    Decimali = minore.toFixed(2);
+} else if (eta >= 65) {
+    over = prezzo - scontoOver;
+    Decimali = over.toFixed(2);
+} else {
+    Decimali = (prezzoDecimali - 0).toFixed(2);
+}
 
 console.log(
     `
@@ -15,18 +26,6 @@ console.log(
     ------Prezzo del biglietto------
         Prezzo pieno: ${prezzoDecimali} €
     ------Totale (con sconto dove disponibile)------
+        Totale (con sconto): ${Decimali} €
     `
 );
-
-if(eta < 18){
-    minore = prezzo - scontoMinore
-    minoreDecimali = minore.toFixed(2)
-    console.log ("Totale (sconto minorenne): " + minoreDecimali + " €");
-} else if(eta >= 65){
-    over = prezzo - scontoOver
-    overDecimali = over.toFixed(2)
-    console.log ("Totale (sconto over 65): " + overDecimali + " €");
-} else{
-    console.log ("Totale: " + prezzoDecimali + " €");
-}
-
