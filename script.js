9b99b00516eed34ed53653d277ae9c86da03b8b7
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-10 17:49:07 +0100

    dichiarate le variabili

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let km = prompt("Inserire quanti chilometri da percorrere");
-let eta = prompt("Inserire età del passeggero");
+let Km = prompt("Inserire quanti chilometri da percorrere");
+let Eta = prompt("Inserire età del passeggero");
 
-let prezzo = km * 0.21;
-let prezzoDecimali = prezzo.toFixed(2);
+let Prezzo = Km * 0.21;
+let PrezzoDecimali = Prezzo.toFixed(2);
 
-let scontoMinore = prezzo * 20 / 100;
-let scontoOver = prezzo * 40 / 100;
+let ScontoMinore = Prezzo * 20 / 100;
+let ScontoOver = Prezzo * 40 / 100;
 
 
-if (eta < 18) {
-    minore = prezzo - scontoMinore;
-    Decimali = minore.toFixed(2);
-} else if (eta >= 65) {
-    over = prezzo - scontoOver;
-    Decimali = over.toFixed(2);
+if (Eta < 18) {
+    let Minore = Prezzo - ScontoMinore;
+    let Decimali = Minore.toFixed(2);
+} else if (Eta >= 65) {
+    let Over = Prezzo - ScontoOver;
+    let Decimali = Over.toFixed(2);
 } else {
-    Decimali = (prezzoDecimali - 0).toFixed(2);
+    let Decimali = (PrezzoDecimali).toFixed(2);
 }
 
 console.log(
     `
     ------Dati del passeggero------
-        Chilometri del percorso: ${km}
-        Età del passeggero: ${eta}
+        Chilometri del percorso: ${Km}
+        Età del passeggero: ${Eta}
     ------Prezzo del biglietto------
-        Prezzo pieno: ${prezzoDecimali} €
+        Prezzo pieno: ${PrezzoDecimali} €
     ------Totale (con sconto dove disponibile)------
         Totale (con sconto): ${Decimali} €
     `
